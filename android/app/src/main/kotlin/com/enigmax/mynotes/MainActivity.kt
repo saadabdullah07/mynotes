bc3bcc58bf75cfa14d497dfa222723e88e42dd90
saadabdullah07 <saad.iitkgp@gmail.com>
2025-05-11 00:33:02 +0530

    My intitial commit

---
+++ android/app/src/main/kotlin/com/enigmax/mynotes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.enigmax.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: 'Flutter Demo Home Page',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.teal, ),
+
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+                    options: DefaultFirebaseOptions.currentPlatform,
+                  ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user  = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false){
+                print('You are a verified User');
+              }
+              else {
+                print('You need to verify your email');
+              }
+              return const Text('Done');
+            default: 
+            return const Text('Loading....');
+          }
+        },
+      ),
+
+    );
+  } 
+}
